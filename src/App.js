e9f542ff688c4d13097f3ed76c63ee3b8fa1df13
Bharath Seshadri <bharathseshadri@Bharaths-MacBook-Pro.local>
2020-11-26 17:00:27 -0600

    hooks-implementation
    Implemented the useState hook from scratch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ''
+    }
+  }
+
+  handleChange = (event) => {
+    const { target: { value }} = event;
+    this.setState({
+      name: value
+    })
+  }
+
+  render() {
+    const { name } = this.state;
+    return (
+      <div class="name-input">
+        <h1>My name is : {name}</h1>
+        <input type="text" value ={name} onChange = {this.handleChange}/>
+      </div>
+    )
+  }
 }
 
 export default App;
